9abcc19e01a2f07b52266e653e832dee12572d99
Luis Lauro González Heredia <lgonzalez@dextratech.com>
2014-03-20 21:54:30 -0600

    First commit for bank demo app

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+class MainMenu {
+private:
+  string menu = "[H]ome [D]eposit [B]alance [W]ithdrawal [E]xit";
+  static int const MAX_LIMIT = 10000;
+
+public:
+  void showMenu () { cout<<menu<<endl; }
+};
+
+int main () {
+  MainMenu menu;
+  menu.showMenu();
+  return 0;
+}
