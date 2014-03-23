7ac65f27d90564ea7864cf77a14c0bf84d008629
Luis Lauro González Heredia <lgonzalez@dextratech.com>
2014-03-22 18:07:56 -0600

    Move all user-interface related code to new class UserInterface
    Also make userBalance static

--- main.cpp
+++ main.cpp
@@ -5,53 +5,16 @@
 using namespace std;
 
 class UserBank {
-private:
-    const string menu = "[H]ome [D]eposit [B]alance [W]ithdrawal [E]xit";
-    static int const MAX_LIMIT = 10000;
-    enum {HOME='H', DEPOSIT='D', BALANCE='B', WITHDRAWAL='W', EXIT='E'};
-    char userOption;
-
 protected:
+    static int const MAX_LIMIT = 10000;
     static const int MAX_TRIES = 3;
-    int userBalance = 5000;
-    const string triesMsg = "You exceeded the number of tries";
+    static int userBalance;
 
 public:
-    // Clears screen and prints main menu
-    void showMenu () {
-        system("clear");
-        cout << menu << endl;
-    }
-
-    // Reads user input and validates it
-    void readSelection () {
-        static int count = 1;
-        cin >> userOption;
-        switch (userOption) {
-        case HOME:
-            showMenu();
-            readSelection();
-            break;
-        case DEPOSIT:
-        case BALANCE:
-        case WITHDRAWAL:
-            break;
-        case EXIT:
-            return;
-        default:
-            if (count >= MAX_TRIES) {
-                cout << triesMsg << endl;
-                break;
-            }
-            cout << "Unknown option, please try again" << endl;
-            count++;
-            readSelection();
-        }
-    }
-
-    char getSelection() { return userOption; }
-
+    // Set initial balance
+    void setBalance(int i) { userBalance = i; }
 };
+int UserBank::userBalance = 5000;
 
 class UserBalance: public UserBank {
 private:
@@ -63,27 +26,10 @@ public:
 };
 
 class Deposit: public UserBank {
-private:
-    const string enterMsg = "Enter the amount to deposit";
-    const string maxMsg = "You exceeded the allowed limit: ";
-    static const int MAX_LIMIT = 10000;
-    int amount = 0;
-
 public:
-    void addAmount() { userBalance += amount; }
-    void readAmount() {
-        static int count = 1;
-        cout << enterMsg << endl;
-        cin >> amount;
-        if (amount > MAX_LIMIT) { // Try again if max amount is exceeded
-            cout << maxMsg << MAX_LIMIT << endl;
-            if (count >= MAX_TRIES) {
-                cout << triesMsg << endl;
-                return;
-            }
-            count++;
-            readAmount();
-        }
+    void addAmount(int amount) {
+        userBalance += amount;
+        cout << userBalance << endl;
     }
 };
 
@@ -93,7 +39,6 @@ private:
     int coinDenom[3] = {0, 0, 0}; // Coin denominations (20, 10, 5)
     int amount; // amount to withdraw
     int aux; //self explanatory variable name ;)
-    const string withdrawMsg = "Enter the amount to withdraw: ";
     const int bills[4] = {500, 200, 100, 50};
     const int coins[3] = {20, 10, 5};
     const int bSize = 4;
@@ -107,7 +52,7 @@ private:
     void printMoney(const int *denom, int *values, int size) {
         for (int i = 0; i < size; i++) {
             if (values[i] == 0) continue;
-            cout << denom[i] << " - " << values[i] << endl;
+            cout << denom[i] << " -> " << values[i] << endl;
         }
     }
 
@@ -115,11 +60,8 @@ public:
     Withdrawal() { // Constructor initialize class attributes
             aux = amount = 0;
     }
-    void readAmount() {
-        cout << withdrawMsg;
-        cin >> amount;
-        aux = amount;
-    }
+    void setAmount(int a) { aux = amount = a; }
+    void updateBalance() { userBalance -= amount; }
     void calculateBills() {
         for (int i = 0; i < bSize; i++) { compareAndAdd(bills, billDenom, i); }
     }
@@ -144,9 +86,89 @@ public:
     }
 };
 
+class UserInterface: public Deposit, public Withdrawal,
+                     public UserBalance {
+private:
+    const string menu = "[H]ome [D]eposit [B]alance [W]ithdrawal [E]xit";
+    const string triesMsg = "You exceeded the number of tries";
+    const string enterMsg = "Enter the amount to deposit";
+    const string maxMsg = "You exceeded the allowed limit: ";
+    const string withdrawMsg = "Enter the amount to withdraw: ";
+    enum {HOME='H', DEPOSIT='D', BALANCE='B', WITHDRAWAL='W', EXIT='E'};
+    char userOption;
+    int amount;
+
+public:
+    // Clears screen and prints main menu
+    void showMenu() {
+        system("clear");
+        cout << menu << endl;
+    }
+
+    // Reads user input and validates it
+    void readSelection() {
+        static int count = 1;
+        cin >> userOption;
+        switch (userOption) {
+        case HOME:
+            showMenu();
+            readSelection();
+            break;
+        case DEPOSIT:
+            readDepositAmount();
+            addAmount(amount);
+            showMenu();
+            readSelection();
+            break;
+        case BALANCE:
+            printBalance();
+            readSelection();
+            break;
+        case WITHDRAWAL:
+            readWithdrawalAmount();
+            calculateAll();
+            printAll();
+            updateBalance();
+            readSelection();
+            break;
+        case EXIT:
+            return;
+        default:
+            if (count >= MAX_TRIES) {
+                cout << triesMsg << endl;
+                break;
+            }
+            cout << "Unknown option, please try again" << endl;
+            count++;
+            readSelection();
+        }
+    }
+    void readDepositAmount() {
+        static int count = 1;
+        cout << enterMsg << endl;
+        cin >> amount;
+        if (amount > MAX_LIMIT) { // Try again if max amount is exceeded
+            cout << maxMsg << MAX_LIMIT << endl;
+            if (count >= MAX_TRIES) {
+                cout << triesMsg << endl;
+                return;
+            }
+            count++;
+            readDepositAmount();
+        }
+    }
+    void readWithdrawalAmount() {
+        cout << withdrawMsg;
+        cin >> amount;
+        setAmount(amount);
+    }
+
+};
+
+
 int main () {
-    UserBank menu;
-    menu.showMenu();
-    menu.readSelection();
+    UserInterface ui;
+    ui.showMenu();
+    ui.readSelection();
     return 0;
 }
